46db7eeda7752f5ff72ee23efa081cb8bb1521fa
hasiubogdan2@gmail.com <rhymes1>
2016-02-12 00:19:03 +0200

    Test method created and the function for the bird distance created

---
+++ TrainProblem/TrainProblem/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrainProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, BirdDistance(100));
+        }
+        public int BirdDistance(int distance)
+        {
+            return 0;
+        }
+    }
+}
