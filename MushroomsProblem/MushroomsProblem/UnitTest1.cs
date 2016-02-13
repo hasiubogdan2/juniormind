ec45465ccd22b8a68c6f03d08e2338fb00e4f605
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-13 17:01:39 +0200

    Beginning of the mushrooms problem

---
+++ MushroomsProblem/MushroomsProblem/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MushroomsProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { Assert.AreEqual( 100 , howManyRedMushroomsare(100,10));
+
+        }
+        public int howManyRedMushroomsare(int redMushrooms, int whiteMushrooms)
+        {
+
+
+            return 100;
+        }
+
+
+    }
+}
