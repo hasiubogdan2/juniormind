81a376b3b381e90fe9f87452831993a8c13ccaad
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-13 16:49:54 +0200

    Resolved, 5 goats in 5 days 25kg of Fay

--- GoatProblem/GoatProblem/UnitTest1.cs
+++ GoatProblem/GoatProblem/UnitTest1.cs
@@ -10,14 +10,15 @@ namespace GoatProblem
         public void TestMethod1()
         {
 
-            Assert.AreEqual(5, KGgoatsEat(1, 1, 1, 5, 5));
-        }
+            Assert.AreEqual(25, kgOfFayEatenbyqGoats(1, 1, 1, 5, 5));
+        
 
-        public double KGgoatsEat(double Xdays, int Ygoats, double zKG, int Qgoats, double Wdays)
+        }
+        public double kgOfFayEatenbyqGoats(double xDays, int yGoats, double zKG, int qGoats, double wDays)
         {
             double kgOfFay = 0;
 
-            kgOfFay = (Xdays * Qgoats * zKG) / (Wdays * Ygoats);
+            kgOfFay = (wDays * qGoats * zKG) / (xDays * yGoats);
 
             return kgOfFay;
         }
