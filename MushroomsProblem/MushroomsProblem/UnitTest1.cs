97d45b19a8dfc3bd4e1d7384f671041cca59cb9a
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-13 21:38:11 +0200

    I tried another method

--- MushroomsProblem/MushroomsProblem/UnitTest1.cs
+++ MushroomsProblem/MushroomsProblem/UnitTest1.cs
@@ -8,15 +8,19 @@ namespace MushroomsProblem
     {
         [TestMethod]
         public void TestMethod1()
-        { Assert.AreEqual( 100 , howManyRedMushroomsare(100,10,110));
+        { Assert.AreEqual( 100 , howManyRedMushroomsare(10,110));
 
         }
-        public int howManyRedMushroomsare(int redMushrooms, int whiteMushrooms, int totalNumberOfMushrooms)
+        public int howManyRedMushroomsare( int xCoef, int totalNumberOfMushrooms)
 
         {
+            int redMushrooms = 0; int whiteMushrooms = 0;
+
+            redMushrooms = xCoef * whiteMushrooms;
             totalNumberOfMushrooms = redMushrooms + whiteMushrooms;
-            whiteMushrooms = 10; totalNumberOfMushrooms = 110;
-             redMushrooms = totalNumberOfMushrooms-whiteMushrooms;
+            totalNumberOfMushrooms = whiteMushrooms + xCoef * whiteMushrooms;
+            totalNumberOfMushrooms = whiteMushrooms*(xCoef + 1);
+            whiteMushrooms = totalNumberOfMushrooms / (xCoef + 1);
 
             return redMushrooms;
         }
