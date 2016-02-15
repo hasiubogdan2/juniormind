4eea1a6dfc281d16d9e9b83965dce10ede31c55f
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-15 14:56:42 +0200

    Start of the problem with bank rate

---
+++ BankRatePRoblem/BankRatePRoblem/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankRatePRoblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0,bankRatePayedIn4Month(1,1,1));
+        }
+
+        public double bankRatePayedIn4Month( double creditBank, double anualRate, double period)  {
+
+
+            return 0;
+            }
+
+
+
+
+    }
+}
