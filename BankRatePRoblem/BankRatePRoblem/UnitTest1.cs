fa3ff0dc0280e0ab907a01819c4a4950a75ffc66
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-21 12:34:21 +0200

    Don`t know why my test fails...

--- BankRatePRoblem/BankRatePRoblem/UnitTest1.cs
+++ BankRatePRoblem/BankRatePRoblem/UnitTest1.cs
@@ -9,14 +9,31 @@ namespace BankRatePRoblem
         [TestMethod]
         public void rateForFirstMonth()
         {
-            decimal rate = bankRatePayedIntheFourthYear3Month(100, 1, 12, 1);
+            decimal rate = CalculateBankRate(100, 1, 12, 1);
             Assert.AreEqual(101, rate);
         }
 
-        public decimal bankRatePayedIntheFourthYear3Month( decimal creditBank, int period, decimal ratePerYear, int currentMonth)  {
+        [TestMethod]
+        public void rateForSecondMonth()
+        {
+            decimal rate = CalculateBankRate(100, 1, 12, 2);
+            Assert.AreEqual(100, rate);
+        }
+        
+        [TestMethod]
+            public void rateForThirdMonthInTheFourthYear()
+        {
+            decimal rate = CalculateBankRate(96, 48, 12, 3);
+            Assert.AreEqual(3.6m, rate);
+        }
+
+         decimal CalculateBankRate(decimal creditBank, int period, decimal ratePerYear, int currentMonth)
+        {
 
             decimal principal = creditBank / period;
-            decimal exacInterestPerMonth= ratePerYear / 12 / 100;
-            return principal + creditBank * exacInterestPerMonth;
-}
+            decimal exacInterestPerMonth = ratePerYear / 12 / 100;
+            decimal sold = creditBank - (currentMonth - 1) * principal;
+            return principal +sold * exacInterestPerMonth;
+        }
+    }
 }
