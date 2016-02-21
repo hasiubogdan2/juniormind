0625d734e0dff8b67507c4509ed7456b9ec4c515
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-22 01:27:17 +0200

    BEginning of the problem

---
+++ SportManProblem/SportManProblem/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportManProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { Assert.AreEqual(1,howManyLapsDoesTheSportman(1,1));
+}
+        public int howManyLapsDoesTheSportman(int laps,int rounds)
+        {
+
+            laps = rounds;
+
+            int result = laps * rounds;
+
+
+            return result;
+        }
+
+    }
+
+}
