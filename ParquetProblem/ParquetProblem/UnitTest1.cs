10f7825d321e871ee4601fdfde34e4388adf0dad
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-23 23:13:06 +0200

    starting the problem

---
+++ ParquetProblem/ParquetProblem/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParquetProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3,CalculateParquet(3,2,2,1));
+
+        }
+
+    public double CalculateParquet( double nLengthOfTheRoom, double mWidthOfTheRoom, double aLengthOfTheParquet, double bWidthOfTheParquet)
+        {
+            double AreaRoom = nLengthOfTheRoom * mWidthOfTheRoom;
+            double AreaParquet = aLengthOfTheParquet * bWidthOfTheParquet;
+            double calculateParquet = AreaRoom / AreaParquet;
+
+
+            return 3;
+
+        }
+            
+            }
+}
