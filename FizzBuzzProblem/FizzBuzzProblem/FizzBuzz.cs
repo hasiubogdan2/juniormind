38b617a69f65a935c2e1536ee184d4c3a9cda531
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-25 00:03:26 +0200

    Test runs for Fizz

---
+++ FizzBuzzProblem/FizzBuzzProblem/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzProblem
+{
+    [TestClass]
+    public class FizzBuzz
+    {
+        [TestMethod]
+        public void Fizztest()
+        {
+            Assert.AreEqual("Fizz", CalculateFizzBuzz(3));
+        }
+
+        public string CalculateFizzBuzz(int multiplier)
+        {
+            if ((multiplier > 2) && (multiplier % 3 == 0))
+            {
+                return "Fizz";
+            }
+            else return "This is not Fizz";
+
+
+            
+        }
+
+    }
+}
