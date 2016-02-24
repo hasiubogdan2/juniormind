fe686d3b1e030215a7d49f7e6038adbc358ae256
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-24 22:38:24 +0200

    Watermelon test 2

---
+++ ArheologicalProblem/ArheologicalProblem/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArheologicalProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2,CalculateMinimumArea(1,1,3,2,3,4));
+
+
+        }
+
+        [TestMethod]
+        public void MyTestMethod2()
+        {
+            Assert.AreEqual(2, CalculateMinimumArea(5, 7, 3, 2, 3, 4));
+        }
+        public float CalculateMinimumArea(float XofA, float YofA,float XofB,float YofB,float XofC,float YofC)
+        {
+            float delta = XofA * YofB + XofB * YofC + XofC * YofA - YofB * XofC - YofC * XofA - YofA * XofB;
+
+            return delta / 2;
+        }
+    }
+}
---
+++ WatermelonProblem/WatermelonProblem/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WatermelonProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual(true, CalculateIfTheWatermelonIsShared(20));
+
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual(false, CalculateIfTheWatermelonIsShared(21));
+        }
+
+
+        Boolean CalculateIfTheWatermelonIsShared(int watermelonKgs)
+        {
+            if ((watermelonKgs > 3)&&(watermelonKgs%2==0))
+            {
+                return true;
+
+            }    else return false;
+
+            
+          
+        }
+    }
+}
