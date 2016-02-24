7b00f0691667980cade143ac5ab2c9df5c3f7b24
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-24 22:53:24 +0200

    FarmerLandProblem started

---
+++ FarmerLand/FarmerLand/FarmerLand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmerLand
+{
+    [TestClass]
+    public class FarmerLand
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0,CalculateTheLand(100,100));
+        }
+
+        public double CalculateTheLand(double lengthOfTheLand,double areaOfTheNewLand)
+        {
+            return 0;
+        }
+
+    }
+
+}
