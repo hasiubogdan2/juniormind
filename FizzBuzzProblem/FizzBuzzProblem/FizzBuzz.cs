1365b26d593a68bdda7aaf46cabcda2b61fae712
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-25 22:59:15 +0200

    Hmmmm...

--- FizzBuzzProblem/FizzBuzzProblem/FizzBuzz.cs
+++ FizzBuzzProblem/FizzBuzzProblem/FizzBuzz.cs
@@ -11,18 +11,20 @@ namespace FizzBuzzProblem
         {
             Assert.AreEqual("Fizz", CalculateFizzBuzz(3));
         }
+        [TestMethod]
+        public void Buzztest()
+        {
+            Assert.AreEqual("Buzz", CalculateFizzBuzz(10));
+        }
 
-        
 
         public string CalculateFizzBuzz(int multiplier)
         {
             string problemString = "";
-            if (multiplier % 3 == 0)
-            {
-                return "Fizz";
-            }
-            else
-                return "THis is not a Fizz";
+            if (multiplier % 3 == 0) { return "Fizz"};
+            if (multiplier % 5 == 0) { return "Buzz"}
+
+        } else return "THis is not a Fizz or a Buzz"
 
             
         } 
