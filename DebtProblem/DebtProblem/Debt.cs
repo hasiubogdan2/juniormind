2b87843d09988ee70a599a3017c819f66c9444cb
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-26 21:14:23 +0200

    SOmething not good

--- DebtProblem/DebtProblem/Debt.cs
+++ DebtProblem/DebtProblem/Debt.cs
@@ -11,16 +11,25 @@ namespace DebtProblem
         {
             Assert.AreEqual(100.4,CalculateTotalSumOfRent(100,5));
         }
+        [TestMethod]
+        public void CalculateSecondIntervalDaysPenalty()
+        {
+            Assert.AreEqual(100.25, CalculateTotalSumOfRent(100, 20));
+        }
 
         public double CalculateTotalSumOfRent(int rent,int extraDays)
         {
             
             double rentPerDay = rent / extraDays;
-            double penalty = 0.02 * rentPerDay;
-            double totalSumOfRent =  rent + penalty;
-            if ((extraDays >= 1) && (extraDays <= 10)) return totalSumOfRent;
 
-             else return 0;   
+            double penalty1 = 0.02 * rentPerDay;
+            double penalty2 = 0.05 * rentPerDay;
+            
+            double totalSumOfRent =  rent + penalty1;
+            double totalSumOfRent2 = rent + penalty2;
+            if ((extraDays >= 1) && (extraDays <= 10)) return totalSumOfRent;
+            if ((extraDays >= 11) && (extraDays <= 30)) return totalSumOfRent2;
+            else return 0;   
         }
     }
 }
