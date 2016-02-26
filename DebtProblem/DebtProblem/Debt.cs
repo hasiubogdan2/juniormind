a25dcbc8ca014ff71b755c364e3e366128126aa1
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-26 21:23:36 +0200

    Now its ok i guess

--- DebtProblem/DebtProblem/Debt.cs
+++ DebtProblem/DebtProblem/Debt.cs
@@ -16,6 +16,11 @@ namespace DebtProblem
         {
             Assert.AreEqual(105, CalculateTotalSumOfRent(100, 20));
         }
+        [TestMethod]
+        public void CalculateRentForLastDays()
+        {
+            Assert.AreEqual(110, CalculateTotalSumOfRent(100, 35));
+        }
 
         public double CalculateTotalSumOfRent(int rent,int extraDays)
         {
@@ -24,11 +29,14 @@ namespace DebtProblem
 
             double penalty1 = 0.02 * rent;
             double penalty2 = 0.05 * rent;
+            double penalty3 = 0.1 * rent;
             
             double totalSumOfRent =  rent + penalty1;
             double totalSumOfRent2 = rent + penalty2;
+            double totalSumOfrent3 = rent + penalty3;
             if ((extraDays >= 1) && (extraDays <= 10)) return totalSumOfRent;
             if ((extraDays >= 11) && (extraDays <= 30)) return totalSumOfRent2;
+            if ((extraDays >= 31) && (extraDays <= 40)) return totalSumOfrent3;
             else return 0;   
         }
     }
