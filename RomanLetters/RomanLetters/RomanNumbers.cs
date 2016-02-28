f24e2973894787b0311b682fc275d4d92cdfc860
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-28 14:36:33 +0200

    Beginning of the problems

---
+++ RomanLetters/RomanLetters/RomanNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanLetters
+{
+    [TestClass]
+    public class RomanNumbers
+    {
+        [TestMethod]
+        public void CalculateFirst10Numbers()
+        {
+            Assert.AreEqual("V",FindTheRomanNumbers(5));
+        }
+
+        public string FindTheRomanNumbers(int number)
+        {
+            string output = "";
+            return "V";
+
+        }
+
+    }
+}
