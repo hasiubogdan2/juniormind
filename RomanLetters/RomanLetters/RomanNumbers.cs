25641a7b9f838a39c46b51b443fb970fc96b2139
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-28 14:55:56 +0200

    Test for numbers

--- RomanLetters/RomanLetters/RomanNumbers.cs
+++ RomanLetters/RomanLetters/RomanNumbers.cs
@@ -7,17 +7,39 @@ namespace RomanLetters
     public class RomanNumbers
     {
         [TestMethod]
-        public void CalculateFirst10Numbers()
+        public void RevealFirst10Numbers()
         {
             Assert.AreEqual("V",FindTheRomanNumbers(5));
         }
 
+        [TestMethod]
+        public void RevealNumber10Test()
+        {
+            Assert.AreEqual("X", FindTheRomanNumbers(10));
+        }
+        [TestMethod]
+        public void RevealNumber9Test()
+        {
+            Assert.AreEqual("IX", FindTheRomanNumbers(9));
+        }
+
         public string FindTheRomanNumbers(int number)
         {
-            string output = "";
-            return "V";
+            string theNumberIs = "";
+            if (number == 1) return  "I";
+            if (number == 2) return  "II";
+            if (number == 3) return  "III";
+            if (number == 4) return  "IV";
+            if (number == 5) return  "V";
+            if (number == 6) return  "VI";
+            if (number == 7) return  "VII";
+            if (number == 8) return  "VIII";
+            if (number == 9) return  "IX";
+            if (number == 10) return  "X";
 
-        }
+            return "not good";
+
+        }    
 
     }
 }
