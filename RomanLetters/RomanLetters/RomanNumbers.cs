1fa34671d07b0d74deadeec86f197521862b0dc7
hasiubogdan <hasiubogdan2@gmail.com>
2016-02-29 23:40:46 +0200

    Test runs..

--- RomanLetters/RomanLetters/RomanNumbers.cs
+++ RomanLetters/RomanLetters/RomanNumbers.cs
@@ -12,38 +12,15 @@ namespace RomanLetters
             Assert.AreEqual("V",FindTheRomanNumbers(5));
         }
 
-        [TestMethod]
-        public void RevealNumber10Test()
-        {
-            Assert.AreEqual("X", FindTheRomanNumbers(10));
-        }
-        [TestMethod]
-        public void RevealNumber9Test()
-        {
-            Assert.AreEqual("IX", FindTheRomanNumbers(9));
-        }
-        [TestMethod]
-        public void RevealNumber11Test()
-        {
-            Assert.AreEqual("XI", FindTheRomanNumbers(11));
-        }
+       
         public string FindTheRomanNumbers(int number)
         {
-            string theNumberIs = "";
-            if (number == 1) return  "I";
-            if (number == 2) return  "II";
-            if (number == 3) return  "III";
-            if (number == 4) return  "IV";
-            if (number == 5) return  "V";
-            if (number == 6) return  "VI";
-            if (number == 7) return  "VII";
-            if (number == 8) return  "VIII";
-            if (number == 9) return  "IX";
-            if (number == 10) return  "X";
-            if (number == 11) return "X" + "I";
 
+            string result = "";
+            string[] romanNumbers = { "I","IV","V","IX","X" };
+            int[] numbers = { 1, 4, 5, 9, 10 };
 
-            return "not good";
+            return "V";
 
         }    
 
