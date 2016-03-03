80dddf5577f817f2e8fa220b8b530c10b9ebd70a
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-03 14:41:03 +0200

    test passes for 90

--- RomanLetters/RomanLetters/RomanNumbers.cs
+++ RomanLetters/RomanLetters/RomanNumbers.cs
@@ -9,33 +9,28 @@ namespace RomanLetters
         [TestMethod]
         public void RevealNumber5Test()
         {
-            Assert.AreEqual("V",FindTheRomanNumbers(5));
+            Assert.AreEqual("XC",FindTheRomanNumbers(90));
         }
 
        
         public string FindTheRomanNumbers(int number)
         {
-
-            string result = string.Empty;
+            string result = "";
             string[] romanNumbers = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C" };
             int[] numbers = { 1, 4, 5, 9, 10, 40, 50, 90, 100 };
-
-           for(int i = numbers.Length-1; i <=100; i--)
-            {
-                while(number-numbers[i]>=0)
+            
+                for (int i = romanNumbers.Length - 1; i >= 0; i--)
                 {
-                    result = number + romanNumbers[i];
-                    number = number - numbers[i];
-                }
+                    while (number - numbers[i] >= 0)
+                    {
 
-            }return result;
-                
-
-
-
-
+                        number =number-numbers[i];
+                        result = result+romanNumbers[i];
+                    }
+                }      return result;       
+            }
 
         }    
 
     }
-}
+
