bbe8fbab4466af5f2e102c6767f8afefadcc8f1f
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-03 18:13:54 +0200

    Test for a chess 3x3 runs !

--- ChessProblem/ChessProblem/SquaresProblem.cs
+++ ChessProblem/ChessProblem/SquaresProblem.cs
@@ -11,6 +11,11 @@ namespace ChessProblem
         {
             Assert.AreEqual(5, CalculateNumberOfSquares(2));
         }
+        [TestMethod]
+        public void CalculateChessTable3x3()
+        {
+            Assert.AreEqual(14, CalculateNumberOfSquares(3));
+        }
         public int CalculateNumberOfSquares(int squares)
         {
             int result = 0;
