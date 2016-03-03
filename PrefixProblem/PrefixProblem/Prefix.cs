78d5d341249eb57d675192576bfb739e13041f79
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-03 19:40:25 +0200

    beginning of the problem

---
+++ PrefixProblem/PrefixProblem/Prefix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrefixProblem
+{
+    [TestClass]
+    public class Prefix
+    {
+        [TestMethod]
+        public void FindCommonPrefix()
+        {
+            Assert.AreEqual("", CalculatePrefix(""));
+        }
+
+        public string CalculatePrefix(string comparison)
+        {
+            string FirstString = string.Empty;
+            string SecondString = string.Empty;
+            return "";
+        }
+    }
+
+}
