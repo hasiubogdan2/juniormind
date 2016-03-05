6e3d6bd6181575aed32624f5e8134abf1f0ccfee
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-05 12:11:33 +0200

    Beginning of the problem

---
+++ PanagramProblem/PanagramProblem/Panagram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PanagramProblem
+{
+    [TestClass]
+    public class Panagram
+    {
+        [TestMethod]
+        public void CalculateIfPanagram()
+        {
+            Assert.AreEqual("NO", FindPanagram("The quick brown fox jumps over the lazy dog"));
+
+        }
+
+        public string FindPanagram(string panagram)
+        {
+            string result = string.Empty;
+
+            return "NO";
+
+        }
+
+    }
+}
