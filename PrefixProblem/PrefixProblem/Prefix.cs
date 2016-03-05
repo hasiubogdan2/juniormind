5e11e927841cdac54b4cdb0aa07a01c007b038b5
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-05 11:32:04 +0200

    Factorial test 1

--- PrefixProblem/PrefixProblem/Prefix.cs
+++ PrefixProblem/PrefixProblem/Prefix.cs
@@ -11,13 +11,29 @@ namespace PrefixProblem
         {
             Assert.AreEqual("", CalculatePrefix("",""));
         }
-
+        [TestMethod]
+        public void CalculateFactorialTest()
+        {
+            Assert.AreEqual(6, CalculateFactorial(3));
+        }
         public string CalculatePrefix(string firstWord, string secondWord)
         {
             int factorial = 0;
             string commonPrefix = string.Empty;
             int i = 0;
+            
             return "";
+        }
+        public int CalculateFactorial(int factNumber)
+        {
+            int factorial = 1;
+            int i = factNumber;
+            while (factNumber >= 1)
+            {
+                factorial *= factNumber ;
+                factNumber--;
+            }
+            return factorial;
         }
     }
 
