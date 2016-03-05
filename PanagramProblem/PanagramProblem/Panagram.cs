43a5dc560113e04001b2471d77eeb3d7e04dd907
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-05 12:37:47 +0200

    I dont know what to do here

--- PanagramProblem/PanagramProblem/Panagram.cs
+++ PanagramProblem/PanagramProblem/Panagram.cs
@@ -9,17 +9,34 @@ namespace PanagramProblem
         [TestMethod]
         public void CalculateIfPanagram()
         {
-            Assert.AreEqual("NO", FindPanagram("The quick brown fox jumps over the lazy dog"));
+            Assert.AreEqual("YES", FindPanagram("The quick brown fox jumps over the lazy dog"));
 
         }
 
         public string FindPanagram(string panagram)
         {
             string result = string.Empty;
+            string[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+        "x", "y", "z"};
+            
+            for(int i = 0; i< panagram.Length; i++)
+            {
+                while (panagram.Length != alphabet.Length)
+                {
+                    return "NO";
+                }
 
-            return "NO";
+            }return "YES";
+
 
         }
 
+
+
+
     }
+
+
 }
+
