c388857824d0240eb2f7f638e1a1e86258a53656
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-05 11:48:07 +0200

    i calculated factorial for nothing actually:)) i was thinking at anagram

--- PrefixProblem/PrefixProblem/Prefix.cs
+++ PrefixProblem/PrefixProblem/Prefix.cs
@@ -9,37 +9,22 @@ namespace PrefixProblem
         [TestMethod]
         public void FindCommonPrefix()
         {
-            Assert.AreEqual("", CalculatePrefix("",""));
+            Assert.AreEqual("aaa", CalculatePrefix("aaa", "aaa"));
         }
-        [TestMethod]
-        public void CalculateFactorialTest()
-        {
-            Assert.AreEqual(6, CalculateFactorial(3));
-        }
-        [TestMethod]
-        public void CalculateFactorialTest2()
-        {
-            Assert.AreEqual(120, CalculateFactorial(5));
-        }
+
         public string CalculatePrefix(string firstWord, string secondWord)
         {
-            int factorial = 0;
+
             string commonPrefix = string.Empty;
-            int i = 0;
-            
-            return "";
-        }
-        public int CalculateFactorial(int factNumber)
-        {
-            int factorial = 1;
-            int i = factNumber;
-            while (factNumber >= 1)
+
+            for (int i = 1; i < firstWord.Length && i < secondWord.Length && firstWord[i] == secondWord[i]; i++)
             {
-                factorial *= factNumber ;
-                factNumber--;
+
+                commonPrefix += firstWord[i];
             }
-            return factorial;
+
+            return commonPrefix;
+
         }
     }
-
-}
+}  
