7ac32da42925fb8d94a4e909cd0ea8078777d42a
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-07 20:56:11 +0200

    Delete alphabet string, return bool

---
+++ ExcelColumnsProblem/ExcelColumnsProblem/ExcelColumns.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelColumnsProblem
+{
+    [TestClass]
+    public class ExcelColumns
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        CalculateColumn
+
+    }
+}
--- PanagramProblem/PanagramProblem/Panagram.cs
+++ PanagramProblem/PanagramProblem/Panagram.cs
@@ -9,36 +9,32 @@ namespace PanagramProblem
         [TestMethod]
         public void CalculateIfPanagram()
         {
-            Assert.AreEqual("No,its not a panagram", FindIfPanagram("b     c"));
+            Assert.AreEqual(false, FindIfPanagram("b     c"));
 
         }
         [TestMethod]
         public void CalculateIfPanagramSentence()
         {
-            Assert.AreEqual("Yes,its a panagram", FindIfPanagram("The quick brown fox jumps over the lazy dog"));
+            Assert.AreEqual(true, FindIfPanagram("The quick brown fox jumps over the lazy dog"));
 
         }
         [TestMethod]
         public void CalculateIfPanagramLetters()
         {
-            Assert.AreEqual("Yes,its a panagram", FindIfPanagram("abcdefghijklmnopqrstuvwxyz"));
+            Assert.AreEqual(true, FindIfPanagram("abcdefghijklmnopqrstuvwxyz"));
         }
-        public string FindIfPanagram(string sentence)
+        public bool FindIfPanagram(string sentence)
         {
             string lowerCaseTransform = sentence.ToLower();
             if (CheckIfPanagram(lowerCaseTransform))
-                return "Yes,its a panagram";
-            else return "No,its not a panagram";
+                return true;
+            else return false;
         }
 
         public bool CheckIfPanagram(string panagram)
         {
-            string[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
-        "x", "y", "z"};
-
-
-            for (int i = 0; i < alphabet.Length; i++)
+            
+            for (int i = 0; i < panagram.Length; i++)
             {
                 while ((char)('a' + i) != panagram[i])
                 {
