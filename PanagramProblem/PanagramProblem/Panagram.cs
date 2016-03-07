c4a061ba735f2374f1f3de2f299c3871ac3616c0
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-07 11:45:30 +0200

    Something is not good...

--- PanagramProblem/PanagramProblem/Panagram.cs
+++ PanagramProblem/PanagramProblem/Panagram.cs
@@ -9,16 +9,25 @@ namespace PanagramProblem
         [TestMethod]
         public void CalculateIfPanagram()
         {
-            Assert.AreEqual(true, FindPanagram("b     c"));
+            Assert.AreEqual(false, CheckIfPanagram("b     c"));
 
         }
         [TestMethod]
         public void CalculateIfPanagramSentence()
         {
-            Assert.AreEqual(true, FindPanagram("The quick brown fox jumps over the lazy dog"));
+            Assert.AreEqual(true, CheckIfPanagram("The quick brown fox jumps over the lazy dog"));
+
         }
 
-        public bool FindPanagram(string panagram)
+        public string FindIfPanagram(string sentence)
+        {
+            string lowerCaseTransform = sentence.ToLower();
+            if (CheckIfPanagram(lowerCaseTransform))
+                return "Yes,its a panagram";
+            else return "No,its not a panagram";
+        }
+
+        public bool CheckIfPanagram(string panagram)
         {
             string[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
         "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
@@ -27,34 +36,15 @@ namespace PanagramProblem
 
             for (int i = 0; i < alphabet.Length; i++)
             {
-                if ((char)('a') == panagram[i]) return true;
-                if ((char)('b') == panagram[i]) return true;
-                if ((char)('c') == panagram[i]) return true;
-                if ((char)('d') == panagram[i]) return true;
-                if ((char)('e') == panagram[i]) return true;
-                if ((char)('f') == panagram[i]) return true;
-                if ((char)('g') == panagram[i]) return true;
-                if ((char)('h') == panagram[i]) return true;
-                if ((char)('i') == panagram[i]) return true;
-                if ((char)('j') == panagram[i]) return true;
-                if ((char)('k') == panagram[i]) return true;
-                if ((char)('l') == panagram[i]) return true;
-                if ((char)('m') == panagram[i]) return true;
-                if ((char)('n') == panagram[i]) return true;
-                if ((char)('o') == panagram[i]) return true;
-                if ((char)('p') == panagram[i]) return true;
-                if ((char)('q') == panagram[i]) return true;
-                if ((char)('r') == panagram[i]) return true;
-                if ((char)('s') == panagram[i]) return true;
-                if ((char)('t') == panagram[i]) return true;
-                if ((char)('u') == panagram[i]) return true;
-                if ((char)('v') == panagram[i]) return true;
-                if ((char)('w') == panagram[i]) return true;
-                if ((char)('x') == panagram[i]) return true;
-                if ((char)('y') == panagram[i]) return true;
-                if ((char)('z') == panagram[i]) return true;
+                while ((char)('a' + i) != panagram[i])
+                {
+                    i++;
+                    return false;
+                }
 
-            } return false;
+
+
+            } return true;
 
 
 }
