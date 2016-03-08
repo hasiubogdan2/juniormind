2a55bd55069c5a3a12f2e24aa258d5393cf68f7b
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-08 11:54:57 +0200

    trying to solve for the A-Z columns

--- ExcelProblem/ExcelProblem/Excel.cs
+++ ExcelProblem/ExcelProblem/Excel.cs
@@ -11,9 +11,22 @@ namespace ExcelProblem
         {
             Assert.AreEqual("A", CalculateColumn(1));
         }
-
+        [TestMethod]
+        public void CalculateForError()
+        {
+            Assert.AreEqual("Error", CalculateColumn(30));
+        }
         public string CalculateColumn(int numberInserted)
         {
+            int i = 0;
+            string output = string.Empty;
+            string[] upperCaseAlphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+                "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z" };
+            for (i = 0; i < upperCaseAlphabet.Length; i++)
+            {
+                if (numberInserted > 26) return "Error";
+            }
+
             return "A";
         }
     }
