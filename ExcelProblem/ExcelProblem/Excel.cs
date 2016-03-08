56e1e9fbc24db70da6042a560ee54f9e0fff3266
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-08 11:40:39 +0200

    Beginning of the problem

---
+++ ExcelProblem/ExcelProblem/Excel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelProblem
+{
+    [TestClass]
+    public class Excel
+    {
+        [TestMethod]
+        public void FindTheColumn()
+        {
+            Assert.AreEqual("A", CalculateColumn(1));
+        }
+
+        public string CalculateColumn(int numberInserted)
+        {
+            return "A";
+        }
+    }
+}
