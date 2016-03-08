c71f51be58741eb5b703c921474d9d1ed5cd99e7
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-08 12:11:30 +0200

    The first 26 tests work

--- ExcelProblem/ExcelProblem/Excel.cs
+++ ExcelProblem/ExcelProblem/Excel.cs
@@ -9,12 +9,23 @@ namespace ExcelProblem
         [TestMethod]
         public void FindTheColumn()
         {
-            Assert.AreEqual("A", CalculateColumn(1));
+            Assert.AreEqual("B", CalculateColumn(1));
         }
         [TestMethod]
         public void CalculateForError()
         {
             Assert.AreEqual("Error", CalculateColumn(30));
+        }
+
+        [TestMethod]
+        public void CalculateForFirst26Columns()
+        {
+            Assert.AreEqual("A", CalculateColumn(0));
+        }
+        [TestMethod]
+        public void CalculateForFirst26Columns2()
+        {
+            Assert.AreEqual("X", CalculateColumn(23));
         }
         public string CalculateColumn(int numberInserted)
         {
@@ -24,10 +35,12 @@ namespace ExcelProblem
                 "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z" };
             for (i = 0; i < upperCaseAlphabet.Length; i++)
             {
-                if ((numberInserted > 26)||(numberInserted<=0)) return "Error";
+                if (numberInserted >= 0 && numberInserted < 26)
+                    return ((char)('A' + numberInserted)).ToString();
             }
 
-            return "A";
+            return "Error";
         }
     }
+
 }
