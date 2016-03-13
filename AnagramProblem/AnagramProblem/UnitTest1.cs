68a158f0be63daf12ceff35be0acfa0bd98d6dd9
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-13 23:53:28 +0200

    Calculate for anagram with same letters (factorial)

--- AnagramProblem/AnagramProblem/UnitTest1.cs
+++ AnagramProblem/AnagramProblem/UnitTest1.cs
@@ -7,16 +7,29 @@ namespace AnagramProblem
     public class UnitTest1
     {
         [TestMethod]
-        public void CalculateAnagramTEst()
+        public void CalculateAnagramWithSameLetters()
         {
-            Assert.AreEqual(6, CalculateAnagram(3));
+            Assert.AreEqual(6, CalculateAnagram("aaa"));
+        }
+        
+        public int CalculateAnagram(string word)
+        {
+
+            return Factorial(3);
+                
         }
 
-        public int CalculateAnagram(int word)
+        int Factorial(int number)
         {
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
 
-            return 6;
+            return result;
+
         }
 
-    }
+    } 
 }
