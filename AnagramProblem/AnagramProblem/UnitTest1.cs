2ca15de23311785c3f6f4102728e2ec9ca9bda91
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-13 23:32:03 +0200

    Beginning of the problem

---
+++ AnagramProblem/AnagramProblem/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramProblem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateAnagramTEst()
+        {
+            Assert.AreEqual(6, CalculateAnagram(3));
+        }
+
+        public int CalculateAnagram(int word)
+        {
+
+            return 6;
+        }
+
+    }
+}
