0fa6daba752392f161a9ae3c5059c67091d6387c
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-28 00:52:15 +0300

    Trying to do GetAt

--- BaseTwoProblem/BaseTwoProblem/BaseTwo.cs
+++ BaseTwoProblem/BaseTwoProblem/BaseTwo.cs
@@ -9,7 +9,7 @@ namespace BaseTwoProblem
         [TestMethod]
         public void ConvertInBaseTwo()
         {
-            CollectionAssert.AreEqual(new byte[] { 0, 1, 0 }, Convert(2));
+            CollectionAssert.AreEqual(new byte[] { 1, 0 }, Convert(2));
             CollectionAssert.AreEqual(new byte[] { 1, 0, 0 }, Convert(4));
             CollectionAssert.AreEqual(new byte[] { 1, 1, 1 }, Convert(7));
         }
@@ -17,19 +17,27 @@ namespace BaseTwoProblem
         [TestMethod]
         public void CalculateAND()
         {
-            CollectionAssert.AreEqual(new byte[] { 0, 0, 0 }, CalculateAND(Convert(1),Convert(2)));
+            CollectionAssert.AreEqual(Convert(1 & 2), CalculateAND(Convert(1),Convert(2)));
+            CollectionAssert.AreEqual(Convert(7 & 2), CalculateAND(Convert(7), Convert(2)));
+        }
+
+        [TestMethod]
+        public void GetAtTEst()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1 }, GetAt(2,new byte[3]));
         }
         byte[] Convert(int number)
         {
-            int position = 0;
+           
             int remainder = 0;
-            byte[] result = new byte[3];
+            byte[] result = new byte[0];
             while (number != 0)
             {
                 remainder = number % 2;
                 number /= 2;
-                result[position] = (byte)remainder;
-                position++;
+                Array.Resize(ref result, result.Length + 1);
+                result[result.Length-1] = (byte)remainder;
+                
             }
             Array.Reverse(result);
             return result;
@@ -38,17 +46,23 @@ namespace BaseTwoProblem
 
         byte[] CalculateAND(byte[] firstBytes,byte[] secondBytes)
         {
-            byte[] result = new byte[3];
+            byte[] result = new byte[firstBytes.Length];
             for(int i=0; i < firstBytes.Length; i++)
             {
                 if (firstBytes[i] == 1 && secondBytes[i] == 1)
                     result[i] = 1;
                 else result[i] = 0;
-                    
+                
+
             }
             return result;      
         }
-
-
+       
+        byte[] GetAt(byte position,byte[] length)
+        {
+            length = new byte[] { 1, 2, 3 };
+            return new byte[length.Length - 1 - position];
+            }
+        }
     }
-}
+
