6c25a8ef839fd6555a30c0d55c3d2855c8577311
hasiubogdan <hasiubogdan2@gmail.com>
2016-03-27 14:48:35 +0300

    Beginning of the problem

---
+++ BaseTwoProblem/BaseTwoProblem/BaseTwo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseTwoProblem
+{
+    [TestClass]
+    public class BaseTwo
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            CollectionAssert.AreEqual(new byte[]{0,1,0},Convert(2));
+        
+        }
+        byte[] Convert(int number)
+        {
+            return null;
+        }
+
+    } 
+}
